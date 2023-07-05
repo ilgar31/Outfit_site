6eb02a43cf84eaf4af26a894bb09341e355690d4
ilgar31 <ilgar.bagishev@gmail.com>
2023-07-05 20:23:45 +0300

    third commit

--- outfit/main/urls.py
+++ outfit/main/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin2/', admin.site.urls),
+    path("", views.index, name='')
 ]
