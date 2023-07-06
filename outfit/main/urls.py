40d4c804cef88b3e490d53090dc7edf2ec4e818b
ilgar31 <ilgar.bagishev@gmail.com>
2023-07-06 16:20:52 +0300

    new pages

--- outfit/main/urls.py
+++ outfit/main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='home')
+    path("", views.index, name='home'),
+    path("goods", views.goods, name='goods'),
+    path('login', views.login, name='login'),
+    path("registration", views.registration, name='registration'),
+
 ]
--- outfit/main/views.py
+++ outfit/main/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
 
-
 def index(request):
     return render(request, "main/index.html")
+
+
+def goods(request):
+    return render(request, "main/goods.html")
+
+
+def registration(request):
+    return render(request, "main/registration.html")
+
+
+def login(request):
+    return render(request, "main/login.html")
